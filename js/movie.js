aa66290a238ae1d40f33bba9adb6eccea399c7e9
beehance <beenyshsara@outlook.com>
2022-01-21 22:04:18 +0500

    Movie app colors scheme updated

--- js/movie.js
+++ js/movie.js
@@ -1,8 +1,8 @@
 // -------------------- API URLs for the App -------------------
 
-const ApiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
-const imageApi = 'https://image.tmdb.org/t/p/w1280';
-const searchApi = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+const ApiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const imageApi = 'https://image.tmdb.org/t/p/w1280'
+const searchApi = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 // ------------------- To integrate DATA into the Page using classes/ids -------------------
 
@@ -12,26 +12,77 @@ const main = document.getElementById('main');
 
 // ------------------ Initiating the MOVIE list -------------------------------
 
-getMovieList(ApiUrl);
+getMovieList(ApiUrl)
 
 // ----------------- Functions ---------------------------
 
-async function getMovieList(url){
-    const movieData = await (await fetch(url)).json();
+// async function getMovieList(url){
+//     const response = await fetch(url)
+//     const movieData = await response.json()
 
-    displayMovies(movieData.results);
+//     displayMovies(movieData.results)
+// }
+
+async function getMovieList(url) {
+    const response = await fetch(url)
+    const moviedata = await response.json()
+
+    displayMovies(moviedata.results)
 }
+// ----------------- To Display MOVIES on the page -------------------------
 
-function displayMovies(movies){
-    main.innerHtml = '';
+
+function displayMovies(movies) {
+    main.innerHTML = ''
 
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview} = movie;
+        const { title, poster_path, vote_average, overview } = movie
 
-        const movieElement = document.createElement('div');
-        movieElement.classList.add('movie');
+        const movieElement = document.createElement('div')
+        movieElement.classList.add('movie')
 
-        
+        movieElement.innerHTML = `
+            <img src="${imageApi + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieElement)
     })
 }
+
+// -------------------- Movie Rating -----------------
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+
+// ------------------------  FORM/ SEARCH ENTRY to catch the MOVIE name ----------------
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const searchMovie = search.value
+
+    if(searchMovie && searchMovie !== '') {
+        getMovieList(searchApi + searchMovie)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
+
 
